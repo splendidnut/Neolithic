f807a8fa32ec38422548567bbbd40719e86dc081
pblackman <ptblackman@gmail.com>
2021-08-05 17:28:06 -0400

    Fixed up var tester a bit.

--- test/test_var_size.c
+++ test/test_var_size.c
@@ -1,17 +1,20 @@
-// this tests the variable sizes
+// this tests the basic variable sizes  (8 bytes)
 bool b      // 1 byte
 char c      // 1 byte
 int i       // 2 bytes
+char *x     // 2 bytes - pointer to char(s)
+int  *y     // 2 bytes - pointer to int(s)
 
+// test structure   (variables should take 25 bytes of space)
 struct Seven {
     char a,b,c
     int d,e
 };
 
-Seven sevenByteStruct   // 7 bytes
-Seven fourteen[2]       // 14 bytes
-Seven *x[2]             // 4 bytes
+Seven sevenByteStruct   // 7 bytes  - single struct
+Seven fourteen[2]       // 14 bytes - two structs
+Seven *x[2]             // 4 bytes  - two pointers to structs
 
 char lastVar
 
-//--- above totals 30 bytes
+//--- above totals 33 bytes
