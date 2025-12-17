46c23eb9e9e518e0eaefd748259710239d1e3a72
pblackman <ptblackman@gmail.com>
2025-12-17 10:41:01 -0500

    Tweak parser to allow more items in a const array.

--- parser/parser.c
+++ parser/parser.c
@@ -47,7 +47,7 @@
 #define MAX_STATEMENTS_IN_BLOCK 2000
 #define MAX_PROG_DECLARATIONS 1000
 #define STRUCT_VAR_LIMIT 200
-#define MAX_ITEMS_IN_LIST 257
+#define MAX_ITEMS_IN_LIST 1025
 #define MAX_SWITCH_CASES 32
 
 //---------------------------
